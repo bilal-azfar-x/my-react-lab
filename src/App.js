6b9440c3c1f7ac32b5dfd0aeeaa0b1a70495e687
bilal-azfar-x <azfarmbilal10@gmail.com>
2024-11-27 00:01:23 +0500

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Greetings from './Components/Greetings.jsx';
+import HelloWorld from './Components/HelloWorld.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Greetings/>
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Greetings.jsx
@@ -0,0 +1,11 @@
+let name = "Bilal Azfar";
+
+function App() {
+    return (
+      <div className="App">
+        <h1>Greetings! My name is {name}.</h1>
+      </div>
+    );
+  }
+  
+  export default App;
