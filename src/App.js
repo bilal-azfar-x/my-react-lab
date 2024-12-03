4e7b1f26285e02e2ba9d68b65895f5432ee6803f
bilal-azfar-x <azfarmbilal10@gmail.com>
2024-12-03 23:54:04 +0500

    third commit (added another product card)

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ function App() {
         title="SONY WH-1000XM5" 
         description="Active Noise Cancelling (ANC), over-ear headphones."
         price="$249.99"
-        imageURL={"https://tinyurl.com/react-headphones"}/>
+        imageURL={"https://www.sony.com/is/image/gwtprod/6145c1d32e6ac8e63a46c912dc33c5bb?fmt=png-alpha&wid=515&hei=515&trf=trim"}/>
+      <ProductCard 
+        title="Soundcore Liberty 4 NC" 
+        description="Active Noise Cancelling (ANC), in-ear headphones."
+        price="$99.99"
+        imageURL={"https://cdn.shopify.com/s/files/1/0516/3761/6830/files/3947Z11.jpg?v=1711008771"}/>
     </div>
   );
   }
